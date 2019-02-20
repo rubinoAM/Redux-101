5202c535775a4a25ac4b50009b2d11eadc85aad2
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-20 15:51:03 -0500

    Students array renders

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,15 +1,43 @@
 import React, {Component} from 'react';
 
+/* This container needs to know about the store so it can use its state
+and update if needed. To accomplish this, we use the connect method from
+react-redux. */
+import { connect } from 'react-redux';
+
+//We need bindActionCreators from Redux to get actions to dispatch to the reducers.
+import { bindActionCreators } from 'redux';
+
 class Student extends Component{
-    constructor(){
+    /* constructor(){
         super()
-    }
+    } */
 
     render(){
+        const students = this.props.rightSideOfRoom.map((student,i)=>{
+            return(
+                <li key={i}>{student}</li>
+            )
+        });
+
         return(
-            <h1>Student</h1>
-        )
+            <div>
+                <h1>Students</h1>
+                {students}
+            </div>
+        );
     }
 }
 
-export default Student;
+/*Function to map redux state to this component's props
+Redux state always comes in as props*/
+function mapStateToProps(state){    //state = rootReducer
+    /*This function returns an object.
+    Property will be props in this component and value will be the piece of state in the reducer.*/
+    return{
+        rightSideOfRoom: state.students,
+    }
+}
+
+//export default Student;
+export default connect(mapStateToProps)(Student);
